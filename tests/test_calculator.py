c59f4683f4cd0c1ac73e1e823e3f788f6b700c45
hajar <hajardakkouki@gmail.com>
2026-03-16 11:08:01 +0000

    fix docker token

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -4,4 +4,4 @@ def test_addition():
     assert calculator.addition(5, 3) == 8
 
 def test_multiplication():
-    assert calculator.multiplication(5, 3) == 15
+    assert calculator.multiplication(4, 2) == 8
